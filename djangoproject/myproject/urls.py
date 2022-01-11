f443c1327a1c826fd124a78e99f729676e4f139f
Haena <kimhn200605@gmail.com>
2022-01-11 16:30:00 +0900

    회원가입/로그인 버튼 페이지 이동

--- djangoproject/myproject/urls.py
+++ djangoproject/myproject/urls.py
@@ -5,12 +5,8 @@ from myapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('about/', about, name="about"),
-    
     path('tips/', tips, name="tips"),
-    
     path('community/', community, name="comuunity"),
-    
     path('share/', share, name="share"),
     path('mypage/', mypage, name="mypage"),
     path('signup/', signup, name="signup"),
