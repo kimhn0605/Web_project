4ad7b655d77691fc44322824bed303ccda000977
Haena <kimhn200605@gmail.com>
2022-01-11 11:08:20 +0900

    상단바 버튼별 페이지 이동

--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -4,3 +4,19 @@ from .models import Fuser
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def tips(request):
+    return render(request, 'tips.html')
+
+def community(request):
+    return render(request, 'community.html')
+
+def share(request):
+    return render(request, 'share.html')
+
+
+def mypage(request):
+    return render(request, 'mypage.html')
--- djangoproject/myproject/urls.py
+++ djangoproject/myproject/urls.py
@@ -1,24 +1,17 @@
-"""myproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from myapp.views import home
+from django.urls import path, include
+from myapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', myapp.views.index, name='index'),
     path('', home, name="home"),
+    path('about/', about, name="about"),
+    
+    path('tips/', tips, name="tips"),
+    
+    path('community/', community, name="comuunity"),
+    
+    path('share/', share, name="share"),
+    path('mypage/', mypage, name="mypage"),
+    
 ]
